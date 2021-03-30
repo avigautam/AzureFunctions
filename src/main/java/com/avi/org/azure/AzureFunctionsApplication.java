fb377366c644988cee6ba99864211500b2158b91
Mohit Gautam <avigautam.knit@gmail.com>
2021-03-30 10:10:04 +0530

    Initial commit

---
+++ src/main/java/com/avi/org/azure/AzureFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.avi.org.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureFunctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avi/org/azure/AzureFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avi.org.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureFunctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
